cb636a59629b94438312365094118edfa78764b8
Lukas Lang <lukas.lang@univie.ac.at>
2017-02-16 14:51:18 +0000

    Updated license.

--- test/fitsurfaceTest.m
+++ test/fitsurfaceTest.m
@@ -1,19 +1,19 @@
-% Copyright 2013 Clemens Kirisits and Lukas Lang
+% Copyright 2017 Lukas Lang
 %
-% This file is part of OFD.
+% This file is part of OFCM.
 %
-%    OFD is free software: you can redistribute it and/or modify
+%    OFCM is free software: you can redistribute it and/or modify
 %    it under the terms of the GNU General Public License as published by
 %    the Free Software Foundation, either version 3 of the License, or
 %    (at your option) any later version.
 %
-%    OFD is distributed in the hope that it will be useful,
+%    OFCM is distributed in the hope that it will be useful,
 %    but WITHOUT ANY WARRANTY; without even the implied warranty of
 %    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %    GNU General Public License for more details.
 %
 %    You should have received a copy of the GNU General Public License
-%    along with OFD.  If not, see <http://www.gnu.org/licenses/>.
+%    along with OFCM.  If not, see <http://www.gnu.org/licenses/>.
 function tests = fitsurfaceTest
     tests = functiontests(localfunctions);
 end
--- util/surfmetricderiv.m
+++ util/surfmetricderiv.m
@@ -1,19 +1,19 @@
-% Copyright 2015 Lukas Lang
+% Copyright 2017 Lukas Lang
 %
-% This file is part of OFDM.
+% This file is part of OFCM.
 %
-%    OFDM is free software: you can redistribute it and/or modify
+%    OFCM is free software: you can redistribute it and/or modify
 %    it under the terms of the GNU General Public License as published by
 %    the Free Software Foundation, either version 3 of the License, or
 %    (at your option) any later version.
 %
-%    OFDM is distributed in the hope that it will be useful,
+%    OFCM is distributed in the hope that it will be useful,
 %    but WITHOUT ANY WARRANTY; without even the implied warranty of
 %    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %    GNU General Public License for more details.
 %
 %    You should have received a copy of the GNU General Public License
-%    along with OFDM.  If not, see <http://www.gnu.org/licenses/>.
+%    along with OFCM.  If not, see <http://www.gnu.org/licenses/>.
 function [d1g11, d1g12, d1g21, d1g22, d2g11, d2g12, d2g21, d2g22] = surfmetricderiv(Ns, c, rho, xi)
 %SURFMETRICDERIV Computes partial derivatives of the metric tensor.
 %   
