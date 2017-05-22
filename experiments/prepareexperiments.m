9a2ae17082346f9aaee0d5866201b3991ff5a368
Lukas Lang <ll542@cam.ac.uk>
2017-05-22 11:36:42 +0100

    Updated frames.

--- experiments/prepareexperiments.m
+++ experiments/prepareexperiments.m
@@ -31,7 +31,7 @@ path = fullfile(datapath, 'LSM 16.03.2012');
 file = fullfile(path, strcat(name, '.lsm'));
 
 % Select frames.
-frames = 100:151;
+frames = 50:151;
 
 % Create start date and time.
 startdate = datestr(now, 'yyyy-mm-dd-HH-MM-SS');
